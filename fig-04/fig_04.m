d0081ac871e8c5a1e0ca41a919b6df61c3f11420
rcassani <raymundo.cassani@gmail.com>
2022-06-11 08:18:56 -0400

    Fig4: Update parameters and data

--- fig-04/fig_04.m
+++ fig-04/fig_04.m
@@ -9,40 +9,30 @@
 % Clean speech
 clean_file= './data/speech_clean.wav';
 [clean_x, clean_fs] = audioread(clean_file);
+n_samples = length(clean_x);
 % Reverberant speech
 rebrv_file= './data/speech_reverberant.wav';
 [revrb_x, revrb_fs] = audioread(rebrv_file);
+revrb_x = revrb_x(1:n_samples);
 
 %% Modulation spectrograms
-clean_modspectr = strfft_modulation_spectrogram(clean_x(1: 17152), clean_fs, ...
+clean_modspectr = strfft_modulation_spectrogram(clean_x, clean_fs, ...
                                                 1024, 0.25*1024, 1, [], 16, []);
 
-revrb_modspectr = strfft_modulation_spectrogram(revrb_x(1: 17152), revrb_fs, ...
+revrb_modspectr = strfft_modulation_spectrogram(revrb_x, revrb_fs, ...
                                                 1024, 0.25*1024, 1, [], 16, []);
                                             
-%% AMA
-% Parameters
+%% Plot figure
+% === Figure
+figure('units','normalized','outerposition',[0 0 1 1])
 
-figure()
-ax1 = subplot(2,2,1);
+% === Modulation spectrograms
+ax1 = subplot(1,2,1);
 plot_modulation_spectrogram_data(clean_modspectr, [], [], [], [-105, -65], 'inferno')
 title('Clean neutral speech')
-ax2 = subplot(2,2,2);
+ax2 = subplot(1,2,2);
 plot_modulation_spectrogram_data(revrb_modspectr, [], [], [], [-105, -65], 'inferno')
 title('RT-0.8sec')
 xlabel([ax1, ax2], 'Modulation frequency (Hz)');
 ylabel([ax1, ax2], 'Conventional frequency (Hz)');
-
-%% Atlas
-% [B,F,T] = MODSPECGRAM(    A,       Fs,  ASEWINDOW,BASEOVERLAP,BASENFFT,MODWINDOW,MODNFFT)
-subplot(2,2,3)
-[B,F,T]=modspecgram(clean_x,clean_fs, 1024,0.75*1024, 1024, 64,512);
-imagesc(T,F,20*log10(abs(B)));
-axis xy
-caxis([-10 30])
-colorbar
-colormap('inferno')
-xlabel('Modulation frequency (Hz)')
-ylabel('Acoustic Frequeny (Hz)')
-title('clean')
 
